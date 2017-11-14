75165f16a37a2d8a6d770b2a67c9b65b1f042e2e
Nicolas Ross <nicolas@zebradog.com>
2017-11-14 16:55:34 -0600

    Boyscout: flexbox support, maintainability improvements

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(function(){
+	var $_GET = {};
+	document.location.search.replace(/\??(?:([^=]+)=([^&]*)&?)/g, function () {
+	    function decode(s){ return decodeURIComponent(s.split("+").join(" ")); }
+	    $_GET[decode(arguments[1])] = decode(arguments[2]);
+	});
+	if($_GET['id']) window.location = '/'+$_GET['id'];
+	var id = document.location.toString().substr(document.location.toString().lastIndexOf("/")+1);
+	if((parseFloat(id) == parseInt(id)) && !isNaN(id)){
+		$('#video').html('<iframe src="https://player.vimeo.com/video/'+id+'?byline=0&amp;portrait=0&amp;color=ffffff&amp;autoplay=1" width="1280" height="720" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+		$.getJSON('https://vimeo.com/api/v2/video/'+id+'.json?callback=?',function(v){
+			if(v[0]){
+				var title = v[0]['title'];
+				$('#title').text(title);
+				document.title = 'ZEBRADOG: '+title;
+			}else{
+				$('#title').text('');
+				document.title = 'ZEBRADOG';
+			}
+		});
+	}else{
+		document.title = 'ZEBRADOG';
+		$('#title').text('Please specify a video.');
+	}
+});
